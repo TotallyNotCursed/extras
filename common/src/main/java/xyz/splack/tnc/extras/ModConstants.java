e099d19cc16fb0580d7f701f8008dca52621e7bd
HeySiMoon <splack1@protonmail.com>
2025-09-04 11:35:50 +0200

    Bump mod version to 0.2.2

--- common/src/main/java/xyz/splack/tnc/extras/ModConstants.java
+++ common/src/main/java/xyz/splack/tnc/extras/ModConstants.java
@@ -1,6 +1,6 @@
 package xyz.splack.tnc.extras;
 
 public class ModConstants {
-  public static final String MOD_VERSION = "0.2.1";
+  public static final String MOD_VERSION = "0.2.2";
   public static final String PACK_VERSION_FILE = "packver.txt";
 }
