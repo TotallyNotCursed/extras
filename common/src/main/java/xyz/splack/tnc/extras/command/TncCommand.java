3c1547892fa951be393d5025b80a7c8ff33af0fd
HeySiMoon <splack1@protonmail.com>
2025-10-09 19:19:48 +0200

    Remove UuidCommand

--- common/src/main/java/xyz/splack/tnc/extras/command/TncCommand.java
+++ common/src/main/java/xyz/splack/tnc/extras/command/TncCommand.java
@@ -5,12 +5,10 @@ import net.minecraft.commands.CommandSourceStack;
 import net.minecraft.commands.Commands;
 import xyz.splack.tnc.extras.command.tnc.CapeCommand;
 import xyz.splack.tnc.extras.command.tnc.LocateModOresCommand;
-import xyz.splack.tnc.extras.command.tnc.UuidCommand;
 
 public class TncCommand {
     static void register(CommandDispatcher<CommandSourceStack> dispatcher) {
         var builder = Commands.literal("tnc");
-        UuidCommand.register(builder);
         LocateModOresCommand.register(builder);
         CapeCommand.register(builder);
         dispatcher.register(builder);
